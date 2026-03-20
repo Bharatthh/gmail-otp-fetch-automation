30dbf9cf9bae41eb318f53c1368feab766d3c748
Bharatthh <bharathsha46@gmail.com>
2026-03-20 10:34:30 +0530

    Initial push for otp fetch

--- cucumber.js
+++ cucumber.js
@@ -2,14 +2,14 @@ module.exports = {
   default: {
     require: [
       'src/step-definitions/**/*.ts',
-      'src/hooks/**/*.ts'
+      'src/support/**/*.ts'
     ],
     requireModule: ['ts-node/register'],
-    paths: ['features/**/*.feature'],
-    format: ['progress',
+    paths: ['src/features/**/*.feature'],
+    format: [
+      'progress',
       'json:reports/cucumber.json',
       'html:reports/html-report.html'
     ]
   }
 };
-
--- features/form.feature
+++
@@ -1,7 +0,0 @@
-Feature: Practice Form
-
-  Scenario: Fill Practice Form Successfully
-    Given I open the practice form pages
-    When I enter all required details
-    Then I submit the form
-    
